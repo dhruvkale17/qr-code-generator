f70af1ad8aa896ac065c1157a3b81fac409c8eee
dhruvk <dhruvk0074@gmail.com>
2023-11-10 14:45:09 +0530

    Prototype with qrcode image save at local

---
+++ README.md
@@ -0,0 +1,11 @@
+QR Code Generator
+
+Author - Dhruv Kale
+Description - A flask application to create a qr code for entered URL
+
+Language - Python
+Python libraries - os, flask, validators, qrcodegenerator
+
+
+
+
---
+++ qrcodegenerator.py
@@ -0,0 +1,20 @@
+# Importing library
+import qrcode
+
+def generate(url):
+	# Data to encode
+	url = "GeeksforGeeks"
+
+	# Creating an instance of QRCode class
+	qr = qrcode.QRCode(version = 1,
+					box_size = 10,
+					border = 5)
+
+	# Adding data to the instance 'qr'
+	qr.add_data(url)
+
+	qr.make(fit = True)
+	img = qr.make_image(fill_color = 'red',
+						back_color = 'white')
+
+	img.save('MyQRC.png')
